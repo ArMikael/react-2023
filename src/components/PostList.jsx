eca18b1a82b62b83034d778c42dbd2404ac8cf54
ArMikael <armikael2@gmail.com>
2023-05-03 10:29:02 +0300

    feat(rc-button): Added ui component - rc-button component.

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import PostItem from "./PostItem";
+import RcButton from "./ui/rc-button/RcButton";
 
 const PostList = ({ posts }) => {
     return (
         <div className='post-list'>
+            <RcButton>Add Post</RcButton>
+
             {posts?.map((post) =>
                 <PostItem post={post} key={post.id}></PostItem>
             )}
---
+++ src/components/ui/rc-button/RcButton.jsx
@@ -0,0 +1,11 @@
+import classes from "./RcButton.module.scss";
+
+const RcButton = (props) => {
+    return (
+        <button {...props} className={classes.rcButton}>
+
+        </button>
+    )
+};
+
+export default RcButton;
