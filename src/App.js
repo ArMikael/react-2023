ab4d7cab149044da1525fa260b847f4c98ae4d75
ArMikael <armikael2@gmail.com>
2023-05-07 22:06:27 +0300

    feat(post service): Implemented PostService to fetch posts from json server.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,17 @@
 import Counter from "./components/Counter";
 import '../src/styles/App.scss';
-import React, { useState } from "react";
+import React, {useEffect, useState} from "react";
 import PostList from "./components/PostList";
 import PostForm from "./components/PostForm";
 import PostFilter from "./components/PostFilter";
 import RcModal from "./components/ui/rc-modal/RcModal";
 import RcButton from "./components/ui/rc-button/RcButton";
 import {usePosts} from "./hooks/usePosts";
+import PostService from "./api/PostService";
 
 function App() {
-    const [ posts, setPosts ] = useState([
-        { id: 101, title: 'JavaScript', body: 'Programming Language' },
-        { id: 102, title: 'TypeScript', body: 'Strongly typed programming language that builds on JavaScript' },
-        { id: 103, title: 'ReactJs', body: 'Open-source front-end JavaScript library' },
-    ]);
-
+    const [ posts, setPosts ] = useState([]);
+    const [isPostLoading, setIsPostLoading] = useState(false);
     const [filter, setFilter] = useState({ sort: '', searchQuery: '' });
     const [modal, setModal] = useState(false);
     const sortedAndFilteredPosts = usePosts(posts, filter.sort, filter.searchQuery);
@@ -23,6 +20,15 @@ function App() {
         setPosts([...posts, newPost]);
         setModal(false);
     };
+
+    useEffect(() => getPosts, []);
+
+    async function getPosts() {
+        setIsPostLoading(true);
+        const posts = await PostService.getAll();
+        setPosts(posts);
+        setIsPostLoading(false);
+    }
 
     const removePost = (removedPost) => {
         setPosts(posts.filter(post => post.id !== removedPost.id));
@@ -37,7 +43,12 @@ function App() {
             <PostForm create={createPost} posts={posts}></PostForm>
         </RcModal>
         <PostFilter filter={filter} setFilter={setFilter}></PostFilter>
-        <PostList remove={removePost} posts={sortedAndFilteredPosts}></PostList>
+
+        {!isPostLoading
+            ? <PostList remove={removePost} posts={sortedAndFilteredPosts}></PostList>
+            : <h2>Loading...</h2>
+        }
+
     </div>
   );
 }
